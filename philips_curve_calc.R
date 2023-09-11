87581b538e1d5180c6e50c30967eb70e6f78cb5c
ahnsb5117 <ahnsb5117@gmail.com>
2023-09-10 19:39:00 -0700

    Japan NAIRU, Phillips curve per decade calculation

---
+++ philips_curve_calc.R
@@ -0,0 +1,68 @@
+library(tidyverse)
+library(tidyquant)
+library(janitor)
+
+dat_us <- c("UNRATE", "CPIAUCSL") %>% 
+  tq_get(get = "economic.data", from ="1948-12-31")
+
+
+#Change around the lag
+
+#US
+dat_us <- c("UNRATE", "CPIAUCSL") %>% 
+  tq_get(get = "economic.data", from ="1948-12-31")
+
+dat_infl <- dat_us %>% 
+  pivot_wider(names_from = symbol, values_from = price) %>% 
+  mutate(infl = 100 * (CPIAUCSL - lag(CPIAUCSL, 12)) / lag(CPIAUCSL, 12), 
+        infl_change = infl - lag(infl, 1),
+        decade = paste0(10* as.numeric(substr(year(date), 1,3)), "s")) %>% 
+  drop_na() %>% 
+  clean_names()
+
+ggplot(dat_infl, aes(x = unrate, y = infl)) +
+  geom_point(aes(color = decade), size =2 , alpha =.5) +
+  geom_smooth(se = F, method = "gam", aes (color = decade), formula = y ~ poly(x, 2))+
+  scale_color_brewer(palette = "Set1") +
+  labs(x = "Unemployment (%)", y = "Inflation (%)") +
+  theme_minimal()
+
+#JAPAN
+dat_can <- c("LRUN64TTJPM156S", "JPNCPIALLMINMEI") %>% 
+  tq_get(get = "economic.data", from ="1960-01-01")
+
+dat_infl_can <- dat_can %>% 
+  pivot_wider(names_from = symbol, values_from = price) %>% 
+  mutate(infl = 100 * (JPNCPIALLMINMEI - lag(JPNCPIALLMINMEI, 12)) / lag(JPNCPIALLMINMEI, 12), 
+         infl_change = infl - lag(infl, 1),
+         decade = paste0(10* as.numeric(substr(year(date), 1,3)), "s")) %>% 
+  drop_na() %>% 
+  rename(unrate = LRUN64TTJPM156S)
+
+ggplot(dat_infl_can, aes(x = unrate, y = infl)) +
+  geom_point(aes(color = decade), size =2 , alpha =.5) +
+  geom_smooth(se = F, method = "gam", aes (color = decade), formula = y ~ poly(x, 2))+
+  scale_color_brewer(palette = "Set1") +
+  labs(x = "Unemployment (%)", y = "Inflation (%)") +
+  theme_minimal()
+
+
+#CANADA
+dat_can <- c("LRUNTTTTCAM156S", "CANCPIALLMINMEI") %>% 
+  tq_get(get = "economic.data", from ="1960-01-01")
+
+dat_infl_can <- dat_can %>% 
+  pivot_wider(names_from = symbol, values_from = price) %>% 
+  mutate(infl = 100 * (CANCPIALLMINMEI - lag(CANCPIALLMINMEI, 12)) / lag(CANCPIALLMINMEI, 12), 
+         infl_change = infl - lag(infl, 1),
+         decade = paste0(10* as.numeric(substr(year(date), 1,3)), "s")) %>% 
+  drop_na() %>% 
+  rename(unrate = LRUNTTTTCAM156S)
+
+ggplot(dat_infl_can, aes(x = unrate, y = infl)) +
+  geom_point(aes(color = decade), size =2 , alpha =.5) +
+  geom_smooth(se = F, method = "gam", aes (color = decade), formula = y ~ poly(x, 2))+
+  scale_color_brewer(palette = "Set1") +
+  labs(x = "Unemployment (%)", y = "Inflation (%)") +
+  theme_minimal()
+
--- phillips_curve_NAIRU.R
+++ phillips_curve_NAIRU.R
@@ -6,20 +6,25 @@ library(mFilter) #Library for HP filter
 data_pc <- pdfetch_FRED(c("GDPC1", "UNRATE", "CPIAUCSL", "PPIACO", "CPILFESL"))
 
 # Convert data to quarterly frequency
-data_pc <- to.period(data_pc, period = "quarter", OHLC = FALSE)
-View(data_pc)
+data_pc_xts <- to.period(data_pc, period = "quarter", OHLC = FALSE)
+data_pc_df <- data.frame(data_pc_xts)
+
+data_pc <- cbind(Date = rownames(data_pc_df), data_pc_df)
 
 #Transformations
-data_pc$lgdp <- log(data_pc$GDPC1) # Take logs
-hp_gdp <- hpfilter(data_pc$lgdp, freq = 1600, type="lambda")
-data_pc$gdpgap <- 100*hp_gdp$cycle
-data_pc$l_cpi <- log(data_pc$CPIAUCSL)
-data_pc$l_cpi_core <- log(data_pc$CPILFESL)
-data_pc$l_ppiaco <- log(data_pc$PPIACO)
-data_pc$unrate <- (data_pc$UNRATE)
+data_pc_xts$lgdp <- log(data_pc_xts$GDPC1) # Take logs
+hp_gdp <- hpfilter(data_pc_xts$lgdp, freq = 1600, type="lambda")
+data_pc_xts$gdpgap <- 100*hp_gdp$cycle
+data_pc_xts$l_cpi <- log(data_pc_xts$CPIAUCSL)
+data_pc_xts$l_cpi_core <- log(data_pc_xts$CPILFESL)
+data_pc_xts$l_ppiaco <- log(data_pc_xts$PPIACO)
+data_pc_xts$unrate <- (data_pc_xts$UNRATE)
 
 #Series for plots of the Phillips curve
-data_pc$inflation <- 100*diff(data_pc$l_cpi, 4)
+
+data_pc_xts$inflation <- 100*diff(data_pc$l_cpi, 4)
+
+
 #plot.xts(data_pc$inflation)
 
 #Add recession bars
@@ -38,7 +43,7 @@ recessions.df = read.table(textConnection(
   colClasses=c('Date', 'Date'), header=TRUE)
 
 ggplot() +
-  geom_line(data = data_pc$unrate, aes(x = Index, y = data_pc$unrate, color = "Unemployment"), lwd = 1) +
+  geom_line(data = data_pc, aes(x = Index, y = unrate, color = "Unemployment"), lwd = 1) +
   geom_line(data = data_pc$inflation, aes(x = Index, y = data_pc$inflation, color = "Inflation"), lwd = 1) +
   geom_rect(data=recessions.df, inherit.aes=F, aes(xmin=Peak, xmax=Trough, ymin=-Inf, ymax=+Inf), fill='darkgray', alpha=0.5) +
   theme_classic() +
@@ -60,37 +65,40 @@ ggplot()+
   theme_bw()
 
 #Creating inflation gap
-data_pc$InflationGap <- data_pc$inflation_q-data_pc$infexp
+data_pc$InflationGap <- data_pc$QuarterlyInflation-data_pc$InflationExpectations
+
 ggplot()+
   geom_line(data = data_pc$inflation, aes(x = Index, y = data_pc$QuarterlyInflation, color = "QuarterlyInflation"), lwd = .8)+
   geom_line(data = data_pc$infexp, aes(x = Index, y = data_pc$InflationGap, color = "InflationGap", ), lwd = .8)+
-  labs(title = "Inflation Gap", x = "Quarter", y = "") +
+  labs(title = "Inflation Gap", x = "Quarterly Change", y = "") +
   labs(color="Legend") +
-  theme_bw()
+  scale_x_datetime(date_labels = '%b %Y', date_breaks = '3 months')
+
 
 #Supply shocks
-data_pc$ss1 <- 4*diff(data_pc$l_cpi)*100 - 4*diff(data_pc$l_cpi_core)*100
-data_pc$ss2 <- 100*diff(data_pc$l_ppiaco)
+data_pc$SupplyShock1 <- 4*diff(data_pc$l_cpi)*100 - 4*diff(data_pc$l_cpi_core)*100
+data_pc$SupplyShock2 <- 100*diff(data_pc$l_ppiaco)
 
-model1 <- lm(infgap ~ unrate, data = data_pc)
+model1 <- lm(InflationGap ~ unrate, data = data_pc)
 summary(model1)
 
-model2 <- lm(infgap ~ 0 + gdpgap, data = data_pc)
+model2 <- lm(InflationGap ~ 0 + gdpgap, data = data_pc)
 summary(model2)
 
-model3a <- lm(infgap ~ unrate + ss1, data = data_pc)
+model3a <- lm(InflationGap ~ unrate + SupplyShock1, data = data_pc)
 summary(model3a)
 
-model3b <- lm(infgap ~ unrate + ss2, data = data_pc)
+model3b <- lm(InflationGap ~ unrate + SupplyShock2, data = data_pc)
 summary(model3b)
 
-model4 <- lm(infgap ~ 0 + gdpgap + ss1, data = data_pc)
+model4 <- lm(InflationGap ~ 0 + gdpgap + SupplyShock1, data = data_pc)
 summary(model4)
 
 library(rollRegres)
 data1 <- na.omit(data_pc)
 pc_rolling <- roll_regres(data1$infgap ~ data1$unrate + data1$ss1, width = 40, do_downdates = TRUE)
 data1$slope <- pc_rolling$coefs[1:255, 2:2]
+
 plot.xts(data1$slope)
 
 
