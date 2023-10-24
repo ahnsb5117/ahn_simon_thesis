d680cfeb759302fbf38b030097558c55b19dd1cc
ahnsb5117 <ahnsb5117@gmail.com>
2023-10-24 15:36:08 -0700

    us unemployment by age

--- US_unemployment_by_age.R
+++ US_unemployment_by_age.R
@@ -6,7 +6,6 @@ library(broom)
 library(tidyr)
 library(scales)
 library(dplyr)
-library(cmapplot)
 
 
 #16-24, 24-54, 55-65
@@ -25,12 +24,11 @@ recessions.df = read.table(textConnection(
 
 
 ggplot() +
-  geom_line(data = data_pc$LNS14024887, aes(x = Index, y = data_pc$LNS14024887, color = "16 to 24 years", linetype = "16 to 24 years"), lwd = 1) +
-  geom_line(data = data_pc$LNS14000060, aes(x = Index, y = data_pc$LNS14000060, color = "24 to 54 years", linetype = "24 to 54 years"), lwd = 1) +
-  geom_line(data = data_pc$LNS14024230, aes(x = Index, y = data_pc$LNS14024230, color = "55 years and older", linetype = "55 years and older"), lwd = 1) +
+  geom_line(data = data_pc$LNS14024887, aes(x = Index, y = data_pc$LNS14024887,  linetype = "16 to 24 years"), lwd = 1) +
+  geom_line(data = data_pc$LNS14000060, aes(x = Index, y = data_pc$LNS14000060, linetype = "24 to 54 years"), lwd = 1) +
+  geom_line(data = data_pc$LNS14024230, aes(x = Index, y = data_pc$LNS14024230,linetype = "55 years and older"), lwd = 1) +
   geom_rect(data=recessions.df, inherit.aes=F, aes(xmin=Peak, xmax=Trough, ymin=-Inf, ymax=+Inf), fill='darkgray', alpha=0.5) +
-  theme_classic() +
-  title
+  theme_bw() +
   labs(color="Legend", linetype = "Legend") +
   theme(legend.position="bottom")+
   labs(x = "Year", y = "Unemployment Rate (%)")
